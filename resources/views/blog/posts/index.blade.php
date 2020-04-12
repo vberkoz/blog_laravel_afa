cf0e0f431f20e94a3cdcd8ac11c626dd27b2da28
Vasyl Berkoz <vberkoz@gmail.com>
2020-04-12 15:50:20 +0200

    Add layout

--- resources/views/blog/posts/index.blade.php
+++ resources/views/blog/posts/index.blade.php
@@ -1,9 +1,26 @@
-<table>
-    @foreach($items as $item)
-        <tr>
-            <td>{{$item->id}}</td>
-            <td>{{$item->title}}</td>
-            <td>{{$item->created_at}}</td>
-        </tr>
-    @endforeach
-</table>
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Created at</th>
+                    </tr>
+                    </thead>
+                    @foreach($items as $item)
+                        <tr>
+                            <td scope="row">{{$item->id}}</td>
+                            <td>{{$item->title}}</td>
+                            <td>{{$item->created_at}}</td>
+                        </tr>
+                    @endforeach
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::group(['namespace' => 'Blog', 'prefix' => 'blog'], function () {
    Route::resource('posts', 'PostController')->names('blog.posts');
 });
 
-Route::resource('rest', 'RestTestController')->names('restTest');
+//Route::resource('rest', 'RestTestController')->names('restTest');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
